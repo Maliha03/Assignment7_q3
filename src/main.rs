19e09328e554a9c37f78d5a13be3c056bd3fc2cb
Maliha <maliha.babar97@gmail.com>
2020-01-19 15:32:05 +0500

    Code

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+
+use q33;
+use std::io;
+//mod lib;
+fn main() {
+
+q33::temp::room::ac();
+   
+    let temp : u8 = user_ip();
+    println!("Room temperature = {} C\n", temp);
+  
+}
+
+fn user_ip () -> u8 {
+
+    let mut ip = String::new();
+    io::stdin().read_line(&mut ip).expect("Failed to read line");
+    let input : u8 = ip.trim().parse().expect("Please type a number!"); 
+    input
+}
